34b338d1aa3a552e3eb1f3727080575e3b0b5b06
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-14 18:37:55 +0300

    Solved task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Подключение библиотеки
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+//Создание div-контейнера, в который будем добавлять карточки картинкок
+const divContainerEl = document.querySelector('.gallery');
+
+//Создание разметки
+const markup = createMarkup(galleryItems);
+  
+function createMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return`
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`})
+    .join("");
+}
+
+//Добавляем разметку в ul-контейнер
+divContainerEl.innerHTML = markup;
+
+//Открытие модального окна с опциями через библиотеку SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay: 250,});
